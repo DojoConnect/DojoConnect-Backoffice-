86354bd6df83e2f2c0714675c7bc76292b7c8543
Akorede Adeyanju <adeyanju.akorede1@gmail.com>
2026-01-18 16:34:17 +0100

    initial work on fetching instructor students endpoint

--- backoffice-BE/src/repositories/student.repository.ts
+++ backoffice-BE/src/repositories/student.repository.ts
@@ -1,5 +1,5 @@
 import { InferInsertModel, InferSelectModel, SQL, eq, inArray } from "drizzle-orm";
-import { students, users } from "../db/schema.js";
+import { students, users, classes, classEnrollments } from "../db/schema.js";
 import { Transaction } from "../db/index.js";
 import { returnFirst } from "../utils/db.utils.js";
 
@@ -75,4 +75,17 @@ export class StudentRepository {
       .innerJoin(users, eq(students.studentUserId, users.id))
       .where(inArray(students.id, studentIds));
   };
+
+  static async fetchInstructorStudentsWithUsers(instructorId: string, tx: Transaction) {
+    return await tx
+      .select({
+        student: students,
+        user: users,
+      })
+      .from(students)
+      .innerJoin(users, eq(students.studentUserId, users.id))
+      .innerJoin(classEnrollments, eq(classEnrollments.studentId, students.id))
+      .innerJoin(classes, eq(classEnrollments.classId, classes.id))
+      .where(eq(classes.instructorId, instructorId));
+  }
 }
